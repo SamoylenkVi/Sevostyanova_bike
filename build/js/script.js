2bfbe989018973a85c14e0cba6ff7a5bb827b43a
SamoylenkVi <SamoylenkVi@users.noreply.github.com>
2021-05-26 22:29:40 +0300

    Добавляет секцию с видами велосипедов

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,58 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const headerToggle = document.querySelector('.header__toggle');
+const navigation = document.querySelector('.navigation')
+const headerIcon = document.querySelector('.header__icon');
+const header = document.querySelector('.header')
 
-pageHeader.classList.remove('page-header--nojs');
+function throttle(func, ms) {
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
   }
+
+  return wrapper;
+}
+
+// функция для проверки pageYOffset и накручивания/скручивания соответствующего класса
+const checkYOffset = () => {
+  if (window.pageYOffset > 0) {
+    header.classList.add('header--scroll')
+  } else {
+    header.classList.remove('header--scroll')
+  }
+}
+
+headerToggle.classList.remove('header__toggle--no-js');
+navigation.classList.add('navigation--close')
+
+headerToggle.addEventListener('click', (evt) => {
+  evt.target.classList.toggle('header__toggle--open')
+  headerIcon.classList.toggle('header__icon--open')
+  navigation.classList.toggle('navigation--open')
+  navigation.classList.toggle('navigation--close')
 });
+
+window.addEventListener('scroll', throttle(
+  checkYOffset, // callback функция для проверки pageYOffset и накручивания/скручивания соответствующего класса
+  300
+));
